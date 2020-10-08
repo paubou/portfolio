223f4b14bb3f719d02beba61ac1d5913d185db29
Paul Bouigue <pbouigue@users.noreply.github.com>
2020-10-08 20:51:59 +0200

    Test
    test

---
+++ _posts/2020-09-15-peintures.md
@@ -0,0 +1,13 @@
+---
+layout: posts
+img: "/assets/pret_97.jpg"
+title: peintures de sortie
+categories: accueil
+---
+
+# Just owo
+
+**Hello world**, this is my first Jekyll blog post.
+
+I hope you like it!
+![My helpful screenshot](/assets/pret_97.jpg)
---
+++ scripts/script.js
@@ -0,0 +1,68 @@
+function nextLetter(ch) {
+    if (!ch.match(/[a-z]/i)) return ch;
+    else if (ch === 'Z') return 'A';
+    else if (ch === 'z') return 'a';
+    return String.fromCharCode(ch.charCodeAt(0) + 1);
+}
+$(document).ready(function(){
+  $('.gauche#bienvenue span, .gauche#propos p, .milieu a, .droite a').each(function() {
+      var letters = $(this).text();
+      var nHTML = '';
+      for(var letter of letters) {
+          nHTML+="<span class='x'>"+letter+"</span>";
+      }
+      $(this).html(nHTML);
+      $(".x").hover(function(e) {
+          if (e.type === "mouseenter") $(this).text(nextLetter($(this).text()));
+      });
+})});
+
+function RandomColor() {
+  var colors = {
+  "#979797": true, //gris
+  "#ff1e00": true, //orange
+    "#305b3f": true, //vert
+    "#99733A": true, //or
+  "#5213c5": true //violet
+ }
+ 
+  
+function getRandomKey(collection) {
+    let keys = Array.from(Object.keys(collection));
+    return keys[Math.floor(Math.random() * keys.length)];
+}
+
+var CouleurGauche1= getRandomKey(colors);
+delete colors[CouleurGauche1]
+var CouleurGauche2= getRandomKey(colors);
+delete colors[CouleurGauche2]
+var CouleurDroite1= getRandomKey(colors);
+delete colors[CouleurDroite1]
+var CouleurDroite2= getRandomKey(colors);
+delete colors[CouleurDroite2]
+
+
+function SetColor ( id, color) {
+var elmt = document.getElementById(id);
+ if ( elmt !== null) {
+   elmt.style.backgroundColor = color;
+ }
+}
+  
+ SetColor ("gauche", CouleurGauche1)
+ SetColor ("droite", CouleurDroite1)
+ SetColor ("gauche1", CouleurGauche1)
+ SetColor ("gauche2", CouleurGauche2)
+ SetColor ("droite1", CouleurDroite1)
+  SetColor ("droite2", CouleurDroite2)
+  
+
+function SetLinks() {
+var liens = document.getElementsByClassName('liencolore');
+  for (i = 0, len = liens.length; i < len; i++) {
+ liens[i].style.color = CouleurGauche1;
+  }
+}
+SetLinks()
+}
+
